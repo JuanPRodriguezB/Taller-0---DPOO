777a9be1f829679800e299336c388f5e373d5911
JuanPRodriguezB <jp.rodriguezb20@uniandes.edu.co>
2022-02-01 21:22:02 -0500

    Primer commit

---
+++ Taller0_Olimpicos/src/uniandes/dpoo/taller0/modificacion/Modificacion.java
@@ -0,0 +1,10 @@
+package uniandes.dpoo.taller0.modificacion;
+
+public class Modificacion 
+{
+	public static void main()
+	{
+		Modificacion consola = new Modificacion();
+		System.out.println("Hola, mundo!");
+	}
+}
