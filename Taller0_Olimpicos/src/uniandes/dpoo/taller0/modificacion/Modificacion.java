35497b4a23ac0827ed9024c8cf1eafc335ee261a
JuanPRodriguezB <jp.rodriguezb20@uniandes.edu.co>
2022-02-01 22:11:30 -0500

    Punto 2

--- Taller0_Olimpicos/src/uniandes/dpoo/taller0/modificacion/Modificacion.java
+++ Taller0_Olimpicos/src/uniandes/dpoo/taller0/modificacion/Modificacion.java
@@ -1,10 +1,24 @@
 package uniandes.dpoo.taller0.modificacion;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import uniandes.dpoo.taller0.procesamiento.CalculadoraEstadisticas;
+import uniandes.dpoo.taller0.procesamiento.LoaderOlimpicos;
 
 public class Modificacion 
 {
-	public static void main()
+
+	/**
+	 * Este es el método principal de la aplicación, con el que inicia la ejecución
+	 * de la aplicación
+	 * @throws IOException 
+	 * @throws FileNotFoundException 
+	 */
+	public static void main(String[] args) throws FileNotFoundException, IOException
 	{
-		Modificacion consola = new Modificacion();
 		System.out.println("Hola, mundo!");
+		CalculadoraEstadisticas datos = LoaderOlimpicos.cargarArchivo("./data/atletas.csv");
+		System.out.println(datos.paisConMasMedallistas());
 	}
+	
 }
